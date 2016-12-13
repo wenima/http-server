166cc30517eb351ece28a89d4eab21a714d76ae1
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-13 00:22:52 -0800

    builts for server.py and client.py; passing 3/4 tests

--- src/client.py
+++ src/client.py
@@ -2,6 +2,10 @@
 
 import socket
 
+message = """That's a saving of 6 characters per method. However, I don't believe Bruce proposes this so that he has to type less. I think he's more concerned about the time wasted by programmers (presumably coming from other languages) where the 'self' parameter doesn't need to be specified, and who occasionally forget it (even though they know better -- habit is a powerful force). It's true that omitting 'self' from the parameter list tends to lead to more obscure error messages than forgetting to type 'self.' in front of an instance variable or method reference. Perhaps even worse (as Bruce mentions) is the error message you get when the method is declared correctly but the call has the wrong number of arguments, like in this example given by Bruce: Let me first bring up a few typical arguments that are brought in against Bruce's proposal.
+
+There's a pretty good argument to make that requiring explicit 'self' in the parameter list reinforces the theoretical equivalency between these two ways of calling a method, given """
+
 
 def client(message):
     """Client function."""
@@ -9,16 +13,25 @@ def client(message):
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
+    print('sending the following message:', message, 'to server at: ', socket.gethostbyname('127.0.0.1'))
     client.sendall(message.encode('utf8'))
     buffer_length = 8
     reply_complete = False
-    server_message = ''
+    server_message = []
     while not reply_complete:
         part = client.recv(buffer_length)
-        server_message += part.decode('utf8')
+        server_message.append(part.decode('utf8'))
+        print('Receiving message from server...')
+        print(server_message[-1])
         if len(part) < buffer_length:
             break
+        else:
+            print('Hold on, there is more...Receiving...')
+    final_message = ''.join(server_message)
+    print('The full message is: ', final_message)
     client.close()
-    return server_message
+    return final_message
 
-client('hello world')
+client(message)
+
+#This is an über secret special information about what happens at the CF office after midnight! Please treat with Confidentiality!")
--- src/server.py
+++ src/server.py
@@ -1,32 +1,105 @@
 """Server."""
 
 import socket
+import logging
 
+def main():
+    server()
+
+
+def set_address():
+    address = ('127.0.0.1', 5000)
+    return address
+
+
+def set_server():
+    server = socket.socket(socket.AF_INET,
+                    socket.SOCK_STREAM,
+                    socket.IPPROTO_TCP)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    return server
+
+
+def handle_message(conn, buffer_length):
+    message = ''
+    message_complete = False
+    while not message_complete:
+        part = conn.recv(buffer_length)
+        message += part.decode('utf8')
+        print('message: ', message)
+        print('len part: ', len(part))
+        print('buffer_length: ', buffer_length)
+        if len(part) < buffer_length:
+            print('setting message to complete: ')
+            message_complete = True
+            break
+    print('return message: ', message)
+    return message
 
 def server():
-    """Server."""
-    server = socket.socket(socket.AF_INET,
-                           socket.SOCK_STREAM,
-                           socket.IPPROTO_TCP)
-    address = ('127.0.0.1', 5000)
+    """Initiates the server, binds the server to an address defined above,
+    listens and accepts new connections."""
+    print('entering server')
+    buffer_length = 2048
+    address = set_address()
+    print('address set to: ', address)
+    server = set_server()
     server.bind(address)
     server.listen(1)
-    conn, addr = server.accept()
-    message = ''
+    print('listening on: ', address)
     while True:
-            buffer_length = 8
-            message_complete = False
-            while not message_complete:
-                part = conn.recv(buffer_length)
-                message += part.decode('utf8')
-                if len(part) < buffer_length:
-                    break
-    conn.sendall(message.encode('utf8'))
-    # conn.close()
-    # server.close()
+        try:
+            conn, addr = server.accept()
+            print('Received a connection by: ', addr)
+            message = handle_message(conn, buffer_length)
+        except KeyboardInterrupt:
+            print('Shutting down...')
+            print('Goodbye.')
+            conn.close()
+            server.close()
+            exit()
+        print('Echoing message back: ')
+        conn.sendall(message.encode('utf8'))
+        print('Closing connection for: ', addr)
+        print('Still listening...(Control + C to stop server)')
+        conn.close()
+    print('end: ', message)
 
-server()
 
-    # try:
 
-    # except KeyboardInterrupt:
+
+
+            #
+            # message = ''
+            # message_complete = True
+            # while message_complete:
+            #         buffer_length = 8
+            #         message_complete = False
+            #         while not message_complete:
+            #             part = conn.recv(buffer_length)
+            #     # print(part.decode('utf8'))
+            #             message += part.decode('utf8')
+            #             print(message)
+            #             print(buffer_length)
+            #             print(len(part))
+            #             if len(part) < buffer_length:
+            #                 break
+            #                 message_complete = False
+        #     conn.sendall(message.encode('utf8'))
+        #     conn.close()
+        # except KeyboardInterrupt:
+        #     server.close()
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+        # server()
+    except KeyboardInterrupt:
+        print('Keyboard interrupted, shutting down server...')
+        server.close()
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os.exit(0)
