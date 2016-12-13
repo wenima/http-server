72f92b196bfca522036956f30a30a64cdcb59698
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-12 20:42:36 -0800

    Added setup files

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Setup for the mailroom moduels."""
+
+from setuptools import setup
+
+
+setup(
+    name="http-server",
+    description="Echo server pinging back the input to the client",
+    version=0.1,
+    author="Amos Boldor, Marc Kessler-Wenicker",
+    author_email='',
+    license="MIT",
+    package_dir={'': 'src'},
+    py_modules=['server.py, client.py'],
+    extras_require={
+        "test": ["pytest", "pytest-watch", "pytest-cov", "tox"]
+    },
+    entry_points={
+        "console_scripts": [
+            "server.py = server:main",
+            "client.py = client:main"]
+    }
+)
