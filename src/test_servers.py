fd1e2034696a85c024250962d0fa74223f49f39b
Amos Boldor <amosboldor@gmail.com>
2016-12-26 14:31:19 -0800

    fixed 4 issues removed corpse code used ''.join() vs concatenation replaced str() with .decode() and removed the if else version check test that a unicode character can straddle the edge of the buffer length

--- src/test_servers.py
+++ src/test_servers.py
@@ -3,10 +3,20 @@
 
 
 import pytest
+import sys
+
+
+if sys.version[0] == '3':
+    word = '¥mes¢sage'
+    special_char_8 = "1234567¥"
+else:
+    word = '¥mes¢sage'.decode('utf8')
+    special_char_8 = "1234567¥".decode('utf8')
+
 
 MESSAGES = [
     ['', ''],
-    ['¥mes¢sage', '¥mes¢sage'],
+    ['¥mes¢sage', word],
     ['message', 'message'],
     ['qwertyuiopasdfgh', 'qwertyuiopasdfgh'],
     ['qwertyui', 'qwertyui']
@@ -18,3 +28,9 @@ def test_client(message, output):
     """Test returns messages shorter than one buffer in length."""
     from client import client
     assert client(message)[:-2] == output
+
+
+def test_client_special_char_with_buffer_length():
+    """Test that unicode character can straddle the edge buffer length (8)."""
+    from client import client
+    assert client("1234567¥")[:-2] == special_char_8
